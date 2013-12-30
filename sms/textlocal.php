49a31ab111b2da711fdb8d380537bb17b9993ac9
Robbie <robbie@xcellweb.co.uk>
2013-12-30 14:47:57 +0000

    modified:   sms/textlocal.php

--- sms/textlocal.php
+++ sms/textlocal.php
@@ -11,13 +11,12 @@ $current2 = getcwd();
 $current = str_replace('sms', '', $current2);
 $smsfile = "".$current."sms.php";
 
-
-// Your Text Local Keyword ensure you add space after the keyword
-$smskey = " ";
-
 // Grab Posted Data from textlocal
 $number2 = $_REQUEST['sender'];
 $message2 = $_REQUEST['content'];
+
+// Your Text Local Keyword ensure you add space after the keyword
+$smskey = " ";
 
 // Convert 44 sender to 0
 $ptn = "/^44/";  
@@ -25,7 +24,7 @@ $rpltxt = "0";
 $number = preg_replace($ptn, $rpltxt, $number2);
 
 // Remove keyword 
-$message = str_replace($smskey, " ", $message2);
+$message = str_replace($smskey, "", $message2);
 
 //SMS file to deal with the message
 include ($smsfile);
