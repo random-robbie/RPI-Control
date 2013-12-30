96500f10891156da7ff6c8140fb294a779c7dbb5
Robbie <robbie@xcellweb.co.uk>
2013-12-30 13:37:19 +0000

    modified:   scripts/setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,8 @@
-www-data ALL=(ALL) NOPASSWD: ALL >> /etc/sudoers
-cp pihat /usr/bin/pihat
-chmod 777 /usr/bin/pithat
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install wakeonlan -y
+sudo echo "www-data ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo cp pihat /usr/bin/pihat
+sudo chmod 777 /usr/bin/pihat
 echo "setup complete"
