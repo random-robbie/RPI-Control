347cb09e0f53faceac9d8caa2b44047fa86b1b8d
Robbie <txt3rob@gmail.com>
2015-03-10 09:47:48 +0000

    Jquery Mobile update

--- install/install.php
+++ install/install.php
@@ -21,9 +21,9 @@ echo '
    <head> 
       <title>Install</title> 
       <meta name="viewport" content="width=device-width, initial-scale=1"> 
-	  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
-	  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-	  <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+	 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 	  </head> 
 	<body>
 		<h2><center>Welcome to the RPI-Control Installer</h2>
