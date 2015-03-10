01dd91a5c7323f11844a8e509a4a31d92c7d9eb0
Robbie <txt3rob@gmail.com>
2015-03-10 09:47:29 +0000

    Jquery Mobile update

--- main.php
+++ main.php
@@ -14,9 +14,9 @@ include ('functions.php');
    <head> 
       <title>Home</title> 
       <meta name="viewport" content="width=device-width, initial-scale=1"> 
-	  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
-	  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-	  <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+	  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+	 <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 <script>
  $(document).ready(function(){
  $('button').click(function() {
